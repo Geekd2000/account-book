366c69dcfc18a6b4d42991bf91c3003b2f7daf4d
jiangjunye <877351993@qq.com>
2022-09-18 22:29:00 +0800

    commit

---
+++ src/main/java/com/potatogeek/bookkeepingengine/BookkeepingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.potatogeek.bookkeepingengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookkeepingEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookkeepingEngineApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/potatogeek/bookkeepingengine/poho/User.java
@@ -0,0 +1,20 @@
+package com.potatogeek.bookkeepingengine.poho;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : JiangJunYe
+ * @version : 1.0
+ * @date : 2022/9/18 18:05
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+public class User {
+    private String name;
+    private String userId;
+}
