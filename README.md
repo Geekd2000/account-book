61789c01a94c66b2add90df48c36ab27e2117e03
jiangjunye <877351993@qq.com>
2023-03-11 22:51:41 +0800

    创建各模块

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 
 准备采用 Dubbo + Nacos + Spring Boot + Mybatis Plus + Redis等技术栈实现，边写边学习......
 
+xxljob作为定时任务框架
 ### 目标
 准备在2023年完成开发并上线微信小程序
 
---
+++ book-gateway/src/main/java/com/potatogeek/book/BookGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.potatogeek.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Book Gateway
+ * @author jjy
+ * @date 2023年3月11日 22:50:40
+ */
+@SpringBootApplication
+public class BookGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookGatewayApplication.class, args);
+    }
+}
