aebe76f89ee5044336aa92086d99947a51b47ae5
jiangjunye <877351993@qq.com>
2023-04-28 17:38:20 +0800

    study

--- book-gateway/src/main/java/com/ipotato/book/controller/DemoGatewayController.java
+++ book-gateway/src/main/java/com/ipotato/book/controller/DemoGatewayController.java
@@ -2,6 +2,7 @@ package com.ipotato.book.controller;
 
 import com.ipotato.book.service.DemoGatewayService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -10,7 +11,8 @@ import javax.annotation.Resource;
  * @author junye
  * @date 2023-04-26 20:26
  */
-@RestController("/demo")
+@RestController
+@RequestMapping("/demo")
 public class DemoGatewayController {
 
     @Resource
